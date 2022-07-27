01bc3470e5efb56ea88361d2481334bf83a0d9d7
divonne-c <chaydivonne_10@hotmail.com>
2022-07-27 14:54:21 +0200

    starting point project

---
+++ src/main/java/com/fixyourhabitsbackend/FixyourhabitsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fixyourhabitsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixyourhabitsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixyourhabitsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fixyourhabitsbackend/FixyourhabitsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixyourhabitsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixyourhabitsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
